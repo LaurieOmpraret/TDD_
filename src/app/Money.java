73a6ed7d2cd4cc3c6d9a47878bd8e5289da7c917
Laurie Ompraret <laurie.ompraret@gmail.com>
2015-01-18 22:39:55 +0100

    Chapitre 8 - Abstract

--- src/app/Money.java
+++ src/app/Money.java
@@ -1,10 +1,17 @@
 /**
  * Created by Laurie on 18/01/2015.
  */
-public class Money {
+public abstract class Money {
     protected int amount;
+    static Money dollar(int amount){
+        return new Dollar(amount);
+    }
+    static Money franc(int amount){
+        return new Franc(amount);
+    }
     public boolean equals(Object object) {
         Money money = (Money) object;
         return money.amount == this.amount && getClass().equals(money.getClass());
     }
+    abstract Money times(int multiplier);
 }
